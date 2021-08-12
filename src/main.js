10f39acbfd0a4fb6466d0b0e61ccaa077201bb59
Kelvin Njiraini <njirainikelvin@gmail.com>
2021-08-12 09:54:09 +0300

    base ui added to the app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import { createApp } from 'vue';
+
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseDialog from './components/ui/BaseDialog.vue';
+import BaseSpinner from './components/ui/BaseSpinner';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 const app = createApp(App);
+app.component('base-badge', BaseBadge);
+app.component('base-button', BaseButton);
+app.component('base-card', BaseCard);
+app.component('base-dialog', BaseDialog);
+app.component('base-spinner', BaseSpinner);
 app.use(store);
 app.use(router);
 app.mount('#app');
